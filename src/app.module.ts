def545afc093a1d318b5b08fc4c96b0b7c3f9e14
scerutti <scerutti@proguidemc.com>
2022-10-25 12:04:27 -0300

    incorporacion de dotenv y su respectiva configuracion

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './shared/configuration';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), UsersModule, AuthModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      load:[configuration]
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
